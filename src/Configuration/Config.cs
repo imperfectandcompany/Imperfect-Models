e738eeecb5e573b00a4561e850e2881096e9201f
raz <9876747+razpbrry@users.noreply.github.com>
2024-03-18 00:51:10 -0500

    Add config

---
+++ src/Configuration/Config.cs
@@ -0,0 +1,18 @@
+﻿using CounterStrikeSharp.API.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ImperfectModels.Configuration
+{
+    public class Config : BasePluginConfig
+    {
+        [JsonPropertyName("ConfigVersion")]
+        public override int Version { get; set; } = 1;
+
+        public int DefaultAlpha { get; set; } = 254;
+    }
+}
--- src/ImperfectModels.cs
+++ src/ImperfectModels.cs
@@ -4,6 +4,7 @@ using CounterStrikeSharp.API.Core.Attributes;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
 using CounterStrikeSharp.API.Modules.Admin;
 using CounterStrikeSharp.API.Modules.Commands;
+using ImperfectModels.Configuration;
 using Microsoft.Extensions.Logging;
 using System.Drawing;
 
@@ -11,12 +12,13 @@ using System.Drawing;
 namespace ImperfectModels;
 
 [MinimumApiVersion(80)]
-public class ImperfectModels : BasePlugin
+public class ImperfectModels : BasePlugin, IPluginConfig<Config>
 {
     public override string ModuleName => "Imperfect-Models";
     public override string ModuleVersion => "1.0.0";
     public override string ModuleAuthor => "Imperfect Gamers - raz";
     public override string ModuleDescription => "A plugin for handling player models.";
+    public Config Config { get; set; } = new Config();
 
     private Dictionary<int, CCSPlayerController> ConnectedPlayers = new Dictionary<int, CCSPlayerController>();
 
@@ -63,6 +65,17 @@ public class ImperfectModels : BasePlugin
     public override void Unload(bool hotReload)
     {
         base.Unload(hotReload);
+    }
+
+    public void OnConfigParsed(Config config)
+    {
+        /// Parsed config version needs to match the current config version in case of addition/removal of fields
+        if (config.Version < Config.Version)
+        {
+            Logger.LogWarning("The config version does not match current version: Expected: {0} | Current: {1}", Config.Version, config.Version);
+        }
+
+        Config = config;
     }
 
     private void OnPlayerConnect(CCSPlayerController? player, bool isForBot = false)
