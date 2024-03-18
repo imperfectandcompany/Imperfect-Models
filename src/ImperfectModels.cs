577c0d111d26313a8ae87b1584dce72a23da28bc
raz <9876747+razpbrry@users.noreply.github.com>
2024-03-18 00:40:17 -0500

    Add command to set alpha for all connected players

--- src/ImperfectModels.cs
+++ src/ImperfectModels.cs
@@ -155,7 +155,46 @@ public class ImperfectModels : BasePlugin
             Logger.LogWarning("The number that was input was not correct.");
             commandInfo.ReplyToCommand("The number that you input was not correct. Try a number between 1 and 100.");
         }
+    }
 
-        commandInfo.ReplyToCommand($"Player model alpha set to {alphaPercentage}");
+    // Command for changing all players model alpha (transparency)
+    [ConsoleCommand("css_modelalpha", "Changes the alpha of all player models")]
+    [CommandHelper(minArgs: 1, usage: "<number for alpha ex. 50>", whoCanExecute: CommandUsage.CLIENT_AND_SERVER)]
+    [RequiresPermissions("@css/root")]
+    public void ChangeModelAlphaCommand(CCSPlayerController? player, CommandInfo commandInfo)
+    {
+        /// This argument is 'css_modelalpha'
+        commandInfo.GetArg(0);
+
+        /// This argument is the number for the alpha
+        var alphaPercentage = commandInfo.GetArg(1);
+        int alphaPercentageInt = 0;
+
+        var intParseSuccess = int.TryParse(alphaPercentage, out alphaPercentageInt);
+
+        if (intParseSuccess)
+        {
+            try
+            {
+                foreach (var connectedPlayer in ConnectedPlayers.Values)
+                {
+                    connectedPlayer.PlayerPawn.Value.Render = Color.FromArgb(alphaPercentageInt, 254, 254, 254);
+                    Utilities.SetStateChanged(connectedPlayer.PlayerPawn.Value, "CBaseModelEntity", "m_clrRender");
+                }
+
+                commandInfo.ReplyToCommand($"All player models alpha set to {alphaPercentage}");
+            }
+            catch (Exception ex)
+            {
+                Logger.LogWarning("Something went wrong when setting the player model alpha {message}", ex.Message);
+
+                commandInfo.ReplyToCommand("Something went wrong when setting the player model alpha. Check error logs for more info.");
+            }
+        }
+        else
+        {
+            Logger.LogWarning("The number that was input was not correct.");
+            commandInfo.ReplyToCommand("The number that you input was not correct. Try a number between 1 and 255.");
+        }
     }
 }
